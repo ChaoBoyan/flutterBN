7860320e7ad2f28a5dee1fe551a185294965fdef
yhbj <yanchaobo@yhbj.cn>
2020-07-19 23:24:26 +0800

    sliverAppBar; 滚动的两种监听方式：controller 和 NotificationListener（widgets）

--- flutterBN/lib/main.dart
+++ flutterBN/lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutterBN/GridView.dart';
 import 'package:flutterBN/RichText.dart';
 import 'package:flutterBN/ListBuild.dart';
 
+import 'CustomScrollViewWidgets.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -19,8 +21,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+//        rgba(79,137,255,1)
+//      主题色
+        primarySwatch: Colors.red,
+//        导航栏颜色
+        primaryColor: Color.fromARGB(255, 79, 137, 255),
+        primaryColorBrightness: Brightness.dark,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 //      home: MyHomePage(title: '首页'),
@@ -32,6 +40,42 @@ class MyApp extends StatelessWidget {
 class ControllerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return ScrollViewAddController();
+  }
+}
+
+//监听滚动 controller
+class ScrollViewAddController extends StatefulWidget {
+  @override
+  _ScrollViewWidgetState createState() => _ScrollViewWidgetState();
+}
+
+class _ScrollViewWidgetState extends State<ScrollViewAddController> {
+  ScrollController controller = ScrollController();
+  bool _isTop = false;
+
+  /** 两种方式监听
+   *一 controller 滚动监听：
+   * 1.设置默认偏移量offset
+   * 2.监听滚动的位置
+   *
+   * 二 NotificationListener   -- widgets
+   * 1.开始滚动 和 结束滚动
+   */
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(() {
+//      print("监听滚动....${controller.offset}");
+      setState(() {
+        _isTop = controller.offset >= 1000;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -39,62 +83,60 @@ class ControllerWidget extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 18),
         ),
       ),
-      body: CustomScrollViewWidget(),
+      body: NotificationListener(
+        onNotification: (ScrollNotification notification){
+          if (notification is ScrollStartNotification) {
+            print("开始滚动");
+          }else if (notification is ScrollUpdateNotification){
+//            notification.metrics.pixels 滚动的距离；
+//notification.metrics.maxScrollExtent 总滚动的距离
+            print("正在滚动....");
+          }else if (notification is ScrollEndNotification){
+            print("结束滚动....");
+          }
+          return true;
+        },
+        child: ListView.builder(
+            controller: controller,
+            itemCount: 30,
+            itemBuilder: (BuildContext bct, int index) {
+              return ListTile(
+                leading: IconButton(
+                  icon: Icon(
+                    CupertinoIcons.paw_solid,
+                    size: 30,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                ),
+                title: Text(
+                  "ComeOnBay,$index",
+                  style: TextStyle(color: Colors.black87, fontSize: 16),
+                ),
+                subtitle: Text(
+                  "联系方式：18866660000",
+                  style: TextStyle(color: Colors.grey, fontSize: 14),
+                ),
+              );
+            }),
+      ),
+      floatingActionButton: _isTop
+          ? FloatingActionButton(
+              onPressed: () {
+                controller.animateTo(0, duration: Duration(seconds: 1), curve: Curves.easeInCirc);
+              },
+              child: Icon(Icons.vertical_align_top),
+            )
+          : null,
     );
   }
-}
-
-class CustomScrollViewWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-//    问题一
-//    如果没有tabbar ,那么对于刘海屏，就会遮挡一部分视图 ，加 SafeArea ,可以解决，但是滚动无法利用空白部分，有瑕疵
-//  这就得 使用 SliverSafeArea 包裹slivierView,这样就能在滚动的时候，利用到空白部分了
-//  此时滑动都最底部（iphone无指纹机型)，也会有空白，不会被遮挡
-
-//  问题二
-//  此时 要设置 上下左右 两边的间距 ，使用padding 来包裹CustomScrollView，也会出现顶部空白，跟问题一类似了，
-//  采用类似的思路，就是 对 slivierView下手，利用SliverPadding 来包裹slivierView，
-
-//    SliverSafeArea   和   SliverPadding ，实战常用，效果还很好，注意嵌套的位置
-
-    return CustomScrollView(
-      slivers: [
-        SliverSafeArea(
-//         上下左右，都可以单独设置，是否需要安全间距，默认是有安全距离的
-//        bottom 设置为false ,就可以 在视图下面 拼接 其他视图，间距通过 SliverPadding 就可以完美控制好
-          bottom: false,
-          sliver: SliverPadding(
-            padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-            sliver: SliverGrid(
-                delegate: SliverChildBuilderDelegate((BuildContext bct, int index) {
-                  return Container(
-                    color: Color.fromARGB(255, Random().nextInt(256),
-                        Random().nextInt(256), Random().nextInt(256)),
-                    child: Text("baby $index",style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
-                  );
-                }, childCount: 20),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 8,
-                  mainAxisSpacing: 8,
-                )),
-          ),
-        ),
-        SliverList(delegate: SliverChildBuilderDelegate(
-            (BuildContext bct,int index){
-              return Container(
-                height: 100,
-                color: Color.fromARGB(255, Random().nextInt(256),
-                    Random().nextInt(256), Random().nextInt(256)),
-                child: Text("baby $index",style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
-
-              );
-            },
-          childCount: 6,
-        ),
-        ),
-      ],
-    );
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
   }
 }
